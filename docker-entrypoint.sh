7c54f57ee6d9fa9235fbc38f2634dcdc8079829e
Yi Zhang <realloveyou@foxmail.com>
2015-08-05 01:40:54 +0000

    change

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,8 +9,5 @@ fi
 # set root password and make root loginable
 echo "root:$ROOT_PASSWORD" | chpasswd
 mkdir -p /var/run/sshd
-sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
-echo "export VISIBLE=now" >> /etc/profile
-exec "$@"
+mkdir -p /var/run/sshd && sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config && sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config && sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 
