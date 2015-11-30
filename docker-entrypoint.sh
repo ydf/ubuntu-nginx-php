15d5a3e0bb99a128f4823884288a0302a0e68e86
tesla <tesla@zamplus.com>
2015-11-30 13:01:12 +0800

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-useradd sshd
+mkdir -p /var/run/sshd
 p=`cat /dev/urandom | tr -dc "a-zA-Z0-9_+\~\!\@\#\$\%\^\&\*\(\)"| fold -w 12 |head -n 1` 
 echo -e "$p\n$p" |passwd sshd && echo $p
-
-/usr/sbin/sshd -D
+service sshd restart
